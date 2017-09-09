989c7ddb54c8166077aea6a2b611e6fa365013c2
zvi <zsondhelm@gmail.com>
2017-09-09 23:31:33 +0300

    mc

--- controllers/user-controller.php
+++ controllers/user-controller.php
@@ -2,10 +2,10 @@
 
 class userController{
 
-    // get modol and send it as array to BL create a new user in data base 
-    // get modol and send it as array to  BL to get info baseed on email/user name
-    // get modol and send it as array to  BL to update info baseed on email/user name
-    // (get modol and send it as array to  BL to delete info baseed on email/user name)?
+     // get model and send it as array to BL create a new user in data base 
+    // get model and send it as array to  BL to get info baseed on email/user name
+    // get model and send it as array to  BL to update info baseed on email/user name
+    // (get model and send it as array to  BL to delete info baseed on email/user name)?
 
 
 }
